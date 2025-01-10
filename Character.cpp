ff14ae5bf6d7cf9c9ec2864b878b7c446c2dd3ca
KimSihyeon1525 <rlatlgus1525@naver.com>
2025-01-10 14:12:54 +0900

    Character.cpp
    수정

--- Character.cpp
+++ Character.cpp
@@ -27,14 +27,19 @@ private:
 
     vector<Item*> inventory;
 
-    Character() :
-
-
 public:
+
+    Character(string name = "") : name(name), level(1), heelth(200), int maxHealth(200), attack(30), experience(0), gold(0)
+    {
+        cout << name << " 생성 :" << level << " 레벨, " << "체력: " << heelth << "경험치: " << experience << "골드: " << gold << endl;
+    }
 
     static Character* getInstance(string name = "")
     {
-
+        if (instance == nullptr)
+        {
+            instance = new Character();
+        }
     }
 
     void displayStatus()
